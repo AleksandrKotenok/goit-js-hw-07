ab2907ca712f522eeef25487ed746fe922aa179a
AleksandrKotenok <83491711+AleksandrKotenok@users.noreply.github.com>
2021-09-06 15:07:55 +0300

    first_DONE

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,6 @@
 const elementUl = document.querySelectorAll('.item')
 console.log(`В списке ${elementUl.length} категории.`)
+elementUl.forEach(element => {
+   console.log(`- Категорія:`, element.firstElementChild.textContent)
+   console.log(`- Кількість елементів:`, element.lastElementChild.children.length)
+});
