0a42b53a54478918531376429454e7b4d9e4b62a
AleksandrKotenok <83491711+AleksandrKotenok@users.noreply.github.com>
2021-09-06 15:51:13 +0300

    second_Done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const pointInjection = document.querySelector('#ingredients')
+const listAdd = arr => arr.map(item => {
+  const list = document.createElement('li')
+  list.textContent = item
+ // console.log(list)
+  return list
+})
+pointInjection.append(...listAdd(ingredients))
+
